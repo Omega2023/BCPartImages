48aaaf16406ba8d4bd2c4d5227f0ac63b9404751
Paul Sughroue <psughroue@omega-usa.com>
2026-01-29 11:22:09 -0600

    BC-5449-2026-01-29

--- BCPartImages/BCPartImages.cs
+++ BCPartImages/BCPartImages.cs
@@ -42,7 +42,8 @@ namespace BCPartImages
         //2.0.0.0 BC-4456-Setup Part Images Application on PCs in Kitting
         //2.0.0.1 BC-5072-Updates BC Image Migration Paths to AWS S3Buckets
         //2.0.0.2 BC-5096-New Image Folder Paths to AWS S3Buckets
-        //2.0.0.3
+        //2.1.0.0 BC-5449-BC 26 Upgrade – SQL Script Updates for BC Part Images
+        //2.1.0.1
 
         String Customer = "";
         string BaseSQL = String.Empty; //SQL command that gets data to fill the m_dsWork DataSet
